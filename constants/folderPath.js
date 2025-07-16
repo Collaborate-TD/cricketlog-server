60819f07fe6f0111daf444af4d2f1ad8ce12a4aa
Devarsh Bharatiya <devarshbhartiya2001@gmail.com>
2025-07-16 15:22:53 -0400

    fix: console

--- constants/folderPath.js
+++ constants/folderPath.js
@@ -1,7 +1,8 @@
 const CONTAINER_PATH = process.env.NODE_ENV === 'production' ? '' : 'data/';
+const TEMP = 'data/';
 
 export const FOLDER_PATH = {
-    TMP_PATH: "data/temp",
+    TMP_PATH: `${TEMP}temp`,
     VIDEO_PATH: `${CONTAINER_PATH}videos`,
     PROFILE_PHOTO_PATH: `${CONTAINER_PATH}profile`,
     DRILL_PATH: `${CONTAINER_PATH}drills`,
